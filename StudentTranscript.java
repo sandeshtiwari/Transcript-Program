1a14693829eea44bd09ac46269a639302816f119
Sandesh Tiwari <sandeshtiwari@live.com>
2017-11-21 12:28:18 -0600

    Created a class for StudentTranscript with a constructor

---
+++ StudentTranscript.java
@@ -0,0 +1,10 @@
+public class StudentTranscript
+{
+   private String name;
+   private int CWID;
+   public StudentTranscript(String name, int CWID)
+   {
+      this.name = name;
+      this.CWID = CWID;
+   }
+}
