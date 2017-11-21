28d2da025d2836d107eb6bcb730f79d79cb95976
Sandesh Tiwari <sandeshtiwari@live.com>
2017-11-21 12:41:08 -0600

    established a sql connection and tested it

--- StudentTranscript.java
+++ StudentTranscript.java
@@ -1,10 +1,24 @@
+import java.sql.*;
 public class StudentTranscript
 {
    private String name;
    private int CWID;
+   private Connection con = null;
+   private Statement stmt;
+   private ResultSet result;
+   private String query;
    public StudentTranscript(String name, int CWID)
    {
       this.name = name;
       this.CWID = CWID;
+      try
+      {
+         Class.forName("com.mysql.jdbc.Driver").newInstance();
+         con = DriverManager.getConnection("jdbc:mysql:///UNIVERSITY", "tiwaris1", "CSCI4055");
+      }
+      catch(Exception e)
+      {
+         System.err.println("Exception: " + e.getMessage());
+      }
    }
 }
---
+++ Transcript.java
@@ -0,0 +1,7 @@
+public class Transcript
+{
+   public static void main(String []args)
+   {
+      StudentTranscript student = new StudentTranscript("Sandesh",60000000);
+   }
+}
