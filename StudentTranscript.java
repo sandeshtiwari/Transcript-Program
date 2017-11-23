631b2eceec04e3e1ba5229b298c9286bd6166feb
Sandesh Tiwari <sandeshtiwari@live.com>
2017-11-23 14:10:00 -0600

    added the showSemGrades method to show grades for each sem

--- StudentTranscript.java
+++ StudentTranscript.java
@@ -5,17 +5,15 @@ public class StudentTranscript
    private int CWID;
    private Connection con = null;
    private Statement stmt;
-   private ResultSet result;
-   private String query;
    public StudentTranscript(int CWID)
    {
       try
       {
          Class.forName("com.mysql.jdbc.Driver").newInstance();
          con = DriverManager.getConnection("jdbc:mysql:///UNIVERSITY", "tiwaris1", "CSCI4055");
-         query = "select Fname, Lname from PERSON, STUDENT where PERSON.CWID = STUDENT.CWID and PERSON.CWID ="+ CWID;
+         String query = "select Fname, Lname from PERSON, STUDENT where PERSON.CWID = STUDENT.CWID and PERSON.CWID ="+ CWID;
          stmt = con.createStatement();
-         result = stmt.executeQuery(query);
+         ResultSet result = stmt.executeQuery(query);
          if(!result.next())
          {
             this.name = "NONE";
@@ -25,7 +23,7 @@ public class StudentTranscript
          {
             this.CWID = CWID;
             this.name = result.getString("Fname") + " " + result.getString("Lname");
- 
+         
          }
          /*try
          {
@@ -33,7 +31,7 @@ public class StudentTranscript
                con.close();
          } 
          catch(SQLException e) {}
-   */
+      */
       }
       catch(Exception e)
       {
@@ -43,5 +41,30 @@ public class StudentTranscript
    public String getName()
    {
       return this.name;
+   }
+   public void showSemGrades()
+   {
+      try 
+      {
+         String semQuery = "select round(avg(GRADE),2) as 'GPA', Semester, Year from TAKEN where CWID ="+ this.CWID + " group by Year, Semester;";
+         String number = "select count(Semester) as num from (select Semester from TAKEN where CWID ="+this.CWID+" group by Year, Semester) as nested;";
+         ResultSet result = stmt.executeQuery(number);
+         int countSem = result.getInt("num");
+         result = stmt.executeQuery(semQuery);
+         for(int i = 0; i < countSem; i++)
+         {
+            if(result.next())
+            {
+               String currentSem = result.getString("Semester");
+               String currentYear = result.getString("Year");
+               this.oneSemGrade(currentSem, currentYear);
+            }
+         }
+      }
+      catch(Exception e)
+      {
+         System.err.println("Exception: " + e.getMessage());
+      }
+   
    }
 }
